7ad968feb536689ec0bda83d751a80198c8ff795
Lans12345 <olanalans12345@gmail.com>
2024-09-24 16:00:35 +0800

    added schedule feature

---
+++ lib/utlis/schedule_data.dart
@@ -0,0 +1,101 @@
+List data = [
+  {
+    'day': 'Monday',
+    'time': '6:00AM-12:00NN',
+    'area': '''
+•Grema
+•Ophelia
+•NHA 1 & 2
+•Kubayan 1 & 2
+•Kubayan •Extension
+•City Housing
+'''
+  },
+  {
+    'day': 'Tuesday',
+    'time': '8:00AM-4:00pm',
+    'area': '''
+•Barangay 4
+•Barangay 5
+•Barangay 7
+•Kalasungay
+•City Market
+•Dalwagan
+•Patpat
+•San Jose
+•Laguitas
+•Aglayan
+•Cabangahan
+•Bangcud
+'''
+  },
+  {
+    'day': 'Wednesday',
+    'time': '8AM - 4PM',
+    'area': '''
+•Barangay 1
+•Barangay 10
+•Barangay 11
+•Impalambong
+•BCT CityMarket
+•Silae
+•St. Peter
+•Zamboanguita
+•Indalasa
+•Miglamin
+•Mapulo
+•Caburacanan
+•Kulaman
+•Busdi
+•Miglamin
+'''
+  },
+  {
+    'day': 'Thursday',
+    'time': '6AM - 12NN',
+    'area': '''
+•Barangay 2
+•Barangay 5
+•Sumpong (Right)
+•City Market
+•Barangay 3
+•Barangay 4
+•Sumpong (Left) including army subdivision
+•Aglayan
+•Mapayag
+•Magsaysay
+•Bindolan
+•Lagdic
+•Capitan Anghel
+•Can-ayan
+'''
+  },
+  {
+    'day': 'Friday',
+    'time': '6AM - 12NN',
+    'area': '''
+•Crossing Berean- Caltex
+• Casisang Proper
+• Casisang Highway
+• Azura-Carmenville-Km 4- Loizas
+• Greenfield City Market
+'''
+  },
+  {
+    'day': 'Saturday',
+    'time': '6AM - 10AM',
+    'area': '''
+PART OF CASISANG
+AREA 1
+Mamala Bridge to Crossing Phase 3
+NHA 3 & Extension
+DEPOT
+Bliss A&B
+Avinca Village City Market
+AREA 1
+• Paglaum 1&2
+• NHA 5
+• Landing Public Market
+'''
+  },
+];
